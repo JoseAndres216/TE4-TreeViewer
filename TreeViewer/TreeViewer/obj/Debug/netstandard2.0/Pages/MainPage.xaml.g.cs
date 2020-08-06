f7664b6508e39e9ea705678bef11192a69333a56
Jose Andres <joseandres216@gmail.com>
2020-08-05 19:49:12 -0600

    Made initial view page

--- TreeViewer/TreeViewer/obj/Debug/netstandard2.0/Pages/MainPage.xaml.g.cs
+++ TreeViewer/TreeViewer/obj/Debug/netstandard2.0/Pages/MainPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace TreeViewer {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry txbNumber;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btnAdd;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            txbNumber = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "txbNumber");
+            btnAdd = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btnAdd");
         }
     }
 }
